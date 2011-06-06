357dfe3c0d5023b3758ddd2acdde2d194e8c0837
riopwg <riopwg@piwigo.org>
2011-06-06 09:25:32 +0000

    Update language IT
    git-svn-id: http://piwigo.org/svn/extensions/batch_manager_prefilters@11244 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -3,7 +3,7 @@
 $lang['with tags'] = 'con i tags';
 $lang['with HD'] = 'con HD';
 $lang['without HD'] = 'senza HD';
-/*TODO*/$lang['with author'] = 'with author';
-/*TODO*/$lang['without author'] = 'without author';
+$lang['with author'] = 'con autore';
+$lang['without author'] = 'senza autore';
 
 ?>
