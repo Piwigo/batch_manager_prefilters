ba0082cb09ef3c8d497d4b651bfd21c488fce8fa
patdenice <pat@piwigo.org>
2011-06-07 18:12:15 +0000

    Small bug fixed
    git-svn-id: http://piwigo.org/svn/extensions/batch_manager_prefilters@11258 68402e56-0260-453c-a942-63ccdbb3a9ee

--- main.inc.php
+++ main.inc.php
@@ -66,8 +66,8 @@ function perform_bmp($filter_sets, $prefilter)
 
 function element_set_global_action_bmp($action)
 {
-  if (($_SESSION['bulk_manager_filter']['prefilter'] == 'with tags' and in_array($action, array('add_tags', 'del_tags')))
-    or (in_array($_SESSION['bulk_manager_filter']['prefilter'], array('with author', 'without author')) and $action == 'author'))
+  if ((@$_SESSION['bulk_manager_filter']['prefilter'] == 'with tags' and in_array($action, array('add_tags', 'del_tags')))
+    or (in_array(@$_SESSION['bulk_manager_filter']['prefilter'], array('with author', 'without author')) and $action == 'author'))
   {
     // let's refresh the page because we the current set might be modified
     redirect(get_root_url().'admin.php?page='.$_GET['page']);
