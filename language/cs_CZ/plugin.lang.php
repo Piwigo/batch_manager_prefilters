5a828cf57697fb7fe1626484f8943aafe87c38ba
ddtddt <ddtddt@piwigo.org>
2011-06-22 12:04:08 +0000

    [extensions] - batch_manager_prefilters - CZ (Czech - Česky) Thanks to webprostor.eu
    git-svn-id: http://piwigo.org/svn/extensions/batch_manager_prefilters@11470 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['with tags'] = 's tagy';
+$lang['with HD'] = 's HD';
+$lang['without HD'] = 'bez HD';
+$lang['with author'] = 's autorem';
+$lang['without author'] = 'bez autora';
+
+?>
