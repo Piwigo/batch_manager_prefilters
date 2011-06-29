a586e91062a83aa89efcfbad60a17c4d2ad9c133
ddtddt <ddtddt@piwigo.org>
2011-06-29 18:56:28 +0000

    [extensions] - batch_manager_prefilters - add ES (Español - Spanish) thanks to Dominique Bernardin
    git-svn-id: http://piwigo.org/svn/extensions/batch_manager_prefilters@11577 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['with tags'] = 'con etiquetas';
+$lang['with HD'] = 'con AD';
+$lang['without HD'] = 'sin AD';
+$lang['with author'] = 'con autor';
+$lang['without author'] = 'sin autor';
+
+?>
