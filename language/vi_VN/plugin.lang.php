b8c7831a8f5f221ec4cf6ea66907cada4785ea86
ddtddt <ddtddt@piwigo.org>
2011-07-04 11:00:29 +0000

    [extensions] - batch_manager_prefilters - update VN (Vietnamese - Tiếng Việt) thanks to icy
    git-svn-id: http://piwigo.org/svn/extensions/batch_manager_prefilters@11616 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/vi_VN/plugin.lang.php
+++ language/vi_VN/plugin.lang.php
@@ -1,9 +1,9 @@
 <?php
 
-$lang['with tags'] = 'với thẻ';
-$lang['with HD'] = 'với độ phân giải cao';
-$lang['without HD'] = 'bỏ qua độ phân giải cao';
-$lang['with author'] = 'với tác giả';
-$lang['without author'] = 'bỏ qua tác giả';
+$lang['with tags'] = 'có thông tin về thẻ';
+$lang['with HD'] = 'có phiên bản phân giải cao';
+$lang['without HD'] = 'không có phiên bản phân giải cao';
+$lang['with author'] = 'có thông tin về tác giả';
+$lang['without author'] = 'không có thông tin về tác giả';
 
 ?>
