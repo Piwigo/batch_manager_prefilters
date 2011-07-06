841796c6dfe84cf2cf5645ed30882fa2e4245fcf
ddtddt <ddtddt@piwigo.org>
2011-07-06 07:15:15 +0000

    [extensions] - batch_manager_prefilters - update Ru (Русский - Russian) thanks to yuragal
    git-svn-id: http://piwigo.org/svn/extensions/batch_manager_prefilters@11645 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ru_RU/plugin.lang.php
+++ language/ru_RU/plugin.lang.php
@@ -3,7 +3,7 @@
 $lang['with tags'] = 'с тегами';
 $lang['with HD'] = 'с HD';
 $lang['without HD'] = 'без HD';
-/*TODO*/$lang['with author'] = 'with author';
-/*TODO*/$lang['without author'] = 'without author';
+$lang['with author'] = 'с автором';
+$lang['without author'] = 'без автора';
 
 ?>
