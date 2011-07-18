e0072d3f32d9ea43a0185adfaa65e1ebb9192fb5
ddtddt <ddtddt@piwigo.org>
2011-07-18 19:26:31 +0000

    [extensions] - batch_manager_prefilters - add nl_NL - Nederlands - Dutch thanks to j.commelin
    git-svn-id: http://piwigo.org/svn/extensions/batch_manager_prefilters@11772 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['with tags'] = 'met tags';
+$lang['with HD'] = 'met HD';
+$lang['without HD'] = 'zonder HD';
+$lang['with author'] = 'met auteur';
+$lang['without author'] = 'zonder auteur';
+
+?>
