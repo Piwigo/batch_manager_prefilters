c97361462244bd96614d3b7aab9b07d54207ad2d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-08-24 06:25:05 +0000

    [batch_manager_prefilters] Update ru_RU, thanks to : Konve
    git-svn-id: http://piwigo.org/svn/extensions/batch_manager_prefilters@29258 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ru_RU/plugin.lang.php
+++ language/ru_RU/plugin.lang.php
@@ -2,4 +2,5 @@
 $lang['with tags'] = 'с тегами';
 $lang['with author'] = 'с автором';
 $lang['without author'] = 'без автора';
-?>
+$lang['With no creation date'] = 'Без даты создания';
+$lang['With no title'] = 'Без названия';
