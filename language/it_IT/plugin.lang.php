13de6d27aee326bbb72ff5191a13f08961b25058
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-08-27 04:29:56 +0000

    [batch_manager_prefilters] Update it_IT, thanks to : Ericnet
    git-svn-id: http://piwigo.org/svn/extensions/batch_manager_prefilters@29282 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -2,4 +2,5 @@
 $lang['with tags'] = 'con i tags';
 $lang['with author'] = 'con autore';
 $lang['without author'] = 'senza autore';
-?>
+$lang['With no title'] = 'Senza titolo';
+$lang['With no creation date'] = 'Senza data di creazione';
