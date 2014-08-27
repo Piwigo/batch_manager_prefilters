9440b858927ce950fcddc491ae2cc69de143a8c8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-08-27 18:03:06 +0000

    [batch_manager_prefilters] Update sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/batch_manager_prefilters@29305 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sk_SK/plugin.lang.php
+++ language/sk_SK/plugin.lang.php
@@ -2,4 +2,5 @@
 $lang['with tags'] = 's klúcovými slovami';
 $lang['with author'] = 's autorom';
 $lang['without author'] = 'bez autora';
-?>
+$lang['With no creation date'] = 'Bez dátumu vytvorenia';
+$lang['With no title'] = 'Bez názvu';
