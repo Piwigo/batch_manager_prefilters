39a58b2e9149793dc467f875ea112e3b5b2d44ba
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-08-27 18:03:05 +0000

    [batch_manager_prefilters] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/extensions/batch_manager_prefilters@29304 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/lv_LV/plugin.lang.php
+++ language/lv_LV/plugin.lang.php
@@ -2,4 +2,5 @@
 $lang['with tags'] = 'ar cilmēm (iezīmēm)';
 $lang['with author'] = 'ar autoru';
 $lang['without author'] = 'bez autora';
-?>
+$lang['With no title'] = 'Bez nosaukuma';
+$lang['With no creation date'] = 'Bez izveides datuma';
