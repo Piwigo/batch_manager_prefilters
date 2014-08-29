2ed7dd3cd47748c949bdf7f6b92f40107ca46045
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-08-29 05:04:29 +0000

    [batch_manager_prefilters] Update vi_VN, thanks to : dummy
    git-svn-id: http://piwigo.org/svn/extensions/batch_manager_prefilters@29315 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/vi_VN/plugin.lang.php
+++ language/vi_VN/plugin.lang.php
@@ -2,4 +2,5 @@
 $lang['with tags'] = 'có thông tin về thẻ';
 $lang['with author'] = 'có thông tin về tác giả';
 $lang['without author'] = 'không có thông tin về tác giả';
-?>
+$lang['With no creation date'] = 'Không kèm ngày tạo';
+$lang['With no title'] = 'Không kèm tiêu đề';
