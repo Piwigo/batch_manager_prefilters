87926e24ad24eb1ca58b5933e58387da547c3472
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-08-29 05:04:23 +0000

    [batch_manager_prefilters] Update es_ES, thanks to : petaqui
    git-svn-id: http://piwigo.org/svn/extensions/batch_manager_prefilters@29311 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/plugin.lang.php
+++ language/es_ES/plugin.lang.php
@@ -2,4 +2,5 @@
 $lang['with tags'] = 'con etiquetas';
 $lang['with author'] = 'con autor';
 $lang['without author'] = 'sin autor';
-?>
+$lang['With no creation date'] = 'Sin fecha de creación';
+$lang['With no title'] = 'Sin título';
