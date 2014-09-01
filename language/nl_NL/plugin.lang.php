da11b4627dc197d756465932f7131e421d346f75
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-01 10:28:24 +0000

    [batch_manager_prefilters] Update nl_NL, thanks to : Kees Hessels
    git-svn-id: http://piwigo.org/svn/extensions/batch_manager_prefilters@29354 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -2,4 +2,5 @@
 $lang['with tags'] = 'met tags';
 $lang['with author'] = 'met auteur';
 $lang['without author'] = 'zonder auteur';
-?>
+$lang['With no creation date'] = 'Zonder aanmaakdatum';
+$lang['With no title'] = 'Zonder titel';
