44bc06788cc679b69caae0e2b4503d772f9f7ede
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-12-15 21:13:54 +0000

    Update fr_CA, thanks to ddtddt
    git-svn-id: http://piwigo.org/svn/extensions/batch_manager_prefilters@30762 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/fr_CA/plugin.lang.php
+++ language/fr_CA/plugin.lang.php
@@ -2,4 +2,5 @@
 $lang['with tags'] = 'avec mots clÈs';
 $lang['with author'] = 'avec auteur';
 $lang['without author'] = 'sans auteur';
-?>
+$lang['With no creation date'] = 'Sans date de cr√©ation';
+$lang['With no title'] = 'Sans titre';
