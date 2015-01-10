70c8bdbf440b0cbfa96668ba6c7db61980b9d537
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-01-10 07:12:47 +0000

    Update cs_CZ, thanks to kjus
    git-svn-id: http://piwigo.org/svn/extensions/batch_manager_prefilters@30879 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/cs_CZ/plugin.lang.php
+++ language/cs_CZ/plugin.lang.php
@@ -4,4 +4,5 @@ $lang['with tags'] = 's tagy';
 $lang['with author'] = 's autorem';
 $lang['without author'] = 'bez autora';
 
-?>
+$lang['With no title'] = 'Bez názvu';
+$lang['With no creation date'] = 'Bez data vytvoření';
