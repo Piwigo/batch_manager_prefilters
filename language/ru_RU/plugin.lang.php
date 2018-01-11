bf677749b1502d3e0289f65fb96aeda0ae57a9b2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-01-11 06:47:33 +0100

    Update ru_RU, thanks to Konve

--- language/ru_RU/plugin.lang.php
+++ language/ru_RU/plugin.lang.php
@@ -4,3 +4,5 @@ $lang['with author'] = 'с автором';
 $lang['without author'] = 'без автора';
 $lang['With no creation date'] = 'Без даты создания';
 $lang['With no title'] = 'Без названия';
+$lang['in only one album'] = 'связанные с одним альбомом';
+$lang['in several albums'] = 'связанные с несколькими альбомами';
