04019bcb08d2eeea75f992b00a578507af2ff364
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-01-11 06:47:26 +0100

    Update nl_NL, thanks to Pat2309

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -4,3 +4,5 @@ $lang['with author'] = 'met fotograaf/maker';
 $lang['without author'] = 'zonder fotograaf/maker';
 $lang['With no creation date'] = 'Zonder aanmaakdatum';
 $lang['With no title'] = 'Zonder titel';
+$lang['in only one album'] = 'geassocieerd met slechts één album';
+$lang['in several albums'] = 'geassocieerd met verscheidene albums';
