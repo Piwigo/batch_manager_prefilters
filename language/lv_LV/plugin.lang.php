a9bec3889e6346427c49fb7b8e6f637040a629e5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-01-27 08:37:22 +0100

    Update lv_LV, thanks to agrisans

--- language/lv_LV/plugin.lang.php
+++ language/lv_LV/plugin.lang.php
@@ -4,3 +4,5 @@ $lang['with author'] = 'ar autoru';
 $lang['without author'] = 'bez autora';
 $lang['With no title'] = 'Bez nosaukuma';
 $lang['With no creation date'] = 'Bez izveides datuma';
+$lang['in several albums'] = 'saistīts ar vairākiem albūmiem';
+$lang['in only one album'] = 'saistīts tikai ar vienu albūmu';
