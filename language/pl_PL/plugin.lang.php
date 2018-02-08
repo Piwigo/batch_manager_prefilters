505871cdc35aa20ecb0c271c3acd6a450e54d059
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-02-08 06:06:06 +0100

    Update pl_PL, thanks to Sauron

--- language/pl_PL/plugin.lang.php
+++ language/pl_PL/plugin.lang.php
@@ -4,3 +4,5 @@ $lang['with author'] = 'z autorem';
 $lang['without author'] = 'bez autora';
 $lang['With no creation date'] = 'Bez daty utworzenia';
 $lang['With no title'] = 'Bez tytułu';
+$lang['in only one album'] = 'powiązane z jednym albumem';
+$lang['in several albums'] = 'powiązane z wieloma albumami';
