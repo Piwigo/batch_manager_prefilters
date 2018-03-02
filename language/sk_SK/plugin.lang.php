65671ff6b668dfd53b0e6f32bc610df513a8590a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-02 06:25:24 +0100

    Update sk_SK, thanks to dodo

--- language/sk_SK/plugin.lang.php
+++ language/sk_SK/plugin.lang.php
@@ -4,3 +4,5 @@ $lang['with author'] = 's autorom';
 $lang['without author'] = 'bez autora';
 $lang['With no creation date'] = 'Bez dátumu vytvorenia';
 $lang['With no title'] = 'Bez názvu';
+$lang['in several albums'] = 'spojené do niekoľkých albumov';
+$lang['in only one album'] = 'spojené len do jedného albumu';
