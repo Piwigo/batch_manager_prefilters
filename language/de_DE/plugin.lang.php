7679372271031f3fc8a8d3431d699fc1f1b4246f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-04 21:08:18 +0100

    Update de_DE, thanks to e-dschungel

--- language/de_DE/plugin.lang.php
+++ language/de_DE/plugin.lang.php
@@ -4,3 +4,5 @@ $lang['with author'] = 'mit Autor';
 $lang['without author'] = 'ohne Autor';
 $lang['With no creation date'] = 'Ohne Erstellungsdatum';
 $lang['With no title'] = 'Ohne Titel';
+$lang['in only one album'] = 'mit nur einem Album verknüpft';
+$lang['in several albums'] = 'mit mehreren Alben verknüpft';
