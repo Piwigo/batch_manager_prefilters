57febe33b2fed8695b0313b019e47404c64db297
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-11 16:57:17 +0100

    Update it_IT, thanks to rio

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -4,3 +4,5 @@ $lang['with author'] = 'con autore';
 $lang['without author'] = 'senza autore';
 $lang['With no title'] = 'Senza titolo';
 $lang['With no creation date'] = 'Senza data di creazione';
+$lang['in only one album'] = 'Associato ad un solo album';
+$lang['in several albums'] = 'Associato a più album';
