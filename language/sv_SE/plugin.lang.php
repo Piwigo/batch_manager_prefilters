9e87733b954c9da659f512b9fd2d45ee87358fb8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-03-12 20:39:00 +0100

    Update sv_SE, thanks to adrian_vg

--- language/sv_SE/plugin.lang.php
+++ language/sv_SE/plugin.lang.php
@@ -4,3 +4,5 @@ $lang['with author'] = 'med författare';
 $lang['without author'] = 'utan författare';
 $lang['With no creation date'] = 'Utan skapandedatum';
 $lang['With no title'] = 'Utan titel';
+$lang['in only one album'] = 'tillhör bara ett album';
+$lang['in several albums'] = 'tillhör flera album';
