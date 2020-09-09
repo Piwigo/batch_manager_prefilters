7de5d2a144aa26755a25aa811244cb96f1e5bdf6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-09-09 20:45:38 +0200

    Update cs_CZ, thanks to webprostor

--- language/cs_CZ/plugin.lang.php
+++ language/cs_CZ/plugin.lang.php
@@ -6,3 +6,5 @@ $lang['without author'] = 'bez autora';
 
 $lang['With no title'] = 'Bez názvu';
 $lang['With no creation date'] = 'Bez data vytvoření';
+$lang['in only one album'] = 'přiřazeno k jednomu album';
+$lang['in several albums'] = 'přiřazeno do více album';
