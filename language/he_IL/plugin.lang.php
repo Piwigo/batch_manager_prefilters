4263906ce7c80c38fe2a06308ddd686568bce443
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 18:07:20 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,14 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['With no creation date'] = 'ללא תאריך יצירה';
+$lang['With no title'] = 'ללא כותרת';
+$lang['in only one album'] = 'מקושר לאלבום אחד בלבד';
+$lang['in several albums'] = 'מקושר למספר אלבומים';
+$lang['with author'] = 'עם יוצר';
+$lang['with tags'] = 'עם תגיות';
+$lang['without author'] = 'ללא יוצר';
